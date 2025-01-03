5b8eb585c02c4915f46e6a2c839d958704ee4879
Alina-bai <alina.baishukurova@gmail.com>
2025-01-03 22:22:08 +0400

    Lesson7

---
+++ Lesson7/Employee.java
@@ -0,0 +1,41 @@
+package org.example.Lesson7;
+
+public class Employee {
+    int id;
+    public String surname;
+    int age;
+    private double salary;
+    String department;
+
+    Employee(int id2, String surname2, int age2, double salary2, String department2){
+        id = id2;
+        surname = surname2;
+        age = age2;
+        salary = salary2;
+        department = department2;
+    }
+    public Employee (int id2){
+        id = id2;
+    }
+
+    Employee (String surname2){
+        surname = surname2;
+    }
+    private  Employee (double salary2 ){
+        salary = salary2;
+    }
+ double uvelechenieZarplaty () {
+        salary *= 2;
+        return salary;
+ }
+  void getId(){
+     System.out.println("ID =  " + id);
+ }
+ public void getSurname(){
+     System.out.println("Surname =  " + surname);
+ }
+ private void getSalary(){
+        System.out.println("Salary =  " + salary);
+ }
+
+}
---
+++ Lesson7/EmployeeTest.java
@@ -0,0 +1,21 @@
+package org.example.Lesson7;
+public class EmployeeTest {
+    public static void main(String[] args) {
+        Employee emp1 = new Employee(5);
+        Employee emp2 = new Employee("Ivanov");
+       // Employee emp3 = new Employee(555.5);
+
+        System.out.println(emp1.id);
+        System.out.println(emp1.surname);
+       // System.out.println(emp1.salary);
+
+        System.out.println(emp2.id);
+        System.out.println(emp2.surname);
+        // System.out.println(emp2.salary);
+
+        emp1.getId();
+        emp1.getSurname();
+        //emp1.getSalary();
+
+    }
+}
