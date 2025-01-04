a1369b6987b851714bc5788964fd448f1145b59c
Alina-bai <alina.baishukurova@gmail.com>
2025-01-04 22:25:10 +0400

    Lesson8

---
+++ Lesson8/Zadacha1.java
@@ -0,0 +1,11 @@
+package org.example.Lesson8;
+
+public class Zadacha1 {
+    static double umnojeniye (double a, double b, double c) {
+        return a * b * c;
+    }
+
+    static void deleniye (double a, double b){
+        System.out.println("Целое частное "+ a/b + ": и остаток " + a%b);
+    }
+}
---
+++ Lesson8/Zadacha1Test.java
@@ -0,0 +1,13 @@
+package org.example.Lesson8;
+
+public class Zadacha1Test {
+    public static void main(String[] args) {
+        System.out.println(Zadacha1.umnojeniye(2,3,4));
+        Zadacha1.deleniye(9,2);
+        System.out.println(Zadacha1.umnojeniye(2.5, 3.5, 4));
+        Zadacha1.deleniye(15,4);
+
+    }
+
+
+}
