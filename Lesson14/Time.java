3c64a3460e65cabe61209986faeeab2db03f1876
Alina-bai <alina.baishukurova@gmail.com>
2025-01-15 21:50:09 +0400

    Lesson14

---
+++ Lesson14/Time.java
@@ -0,0 +1,28 @@
+package org.example.Lesson14;
+
+public class Time {
+    static void vremya () {
+        OUTER:
+        for (int chas=0; chas<6; chas++ ) {
+            MIDDLE:
+            for (int minuta = 0; minuta < 60; minuta++) {
+                if (chas > 1 && minuta % 10 ==0) {
+                break OUTER;
+                }
+            INNER:
+            for (int sekunda=0; sekunda<60; sekunda++){
+                if (sekunda*chas > minuta) {
+                    continue MIDDLE;
+
+                }
+                System.out.println(chas +":" + minuta + ":" + sekunda);
+            }
+         }
+    }
+}
+
+    public static void main(String[] args) {
+        vremya();
+    }
+
+}
