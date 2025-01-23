5b1fda7ad63c0566bcbb341ec2e7b5486f32a68b
Alina-bai <alina.baishukurova@gmail.com>
2025-01-23 12:30:25 +0400

    Lesson17

---
+++ Lesson17/Lesson17.java
@@ -0,0 +1,22 @@
+package org.example.Lesson17;
+
+public class Lesson17 {
+    public static boolean ravenstva(StringBuilder sb1, StringBuilder sb2){
+        boolean result = true;
+        if (sb1.length() == sb2.length()) {
+            for (int i=0; i < sb1.length(); i++) {
+                if(sb1.charAt(i) != sb2.charAt(i)) {
+                    result=false;
+                    break;
+                }
+            }
+        } else {
+            result = false;
+        }
+        return result;
+
+    }
+
+}
+
+
---
+++ Lesson17/Lesson17Test.java
@@ -0,0 +1,18 @@
+package org.example.Lesson17;
+
+public class Lesson17Test {
+    public static void main(String[] args) {
+        StringBuilder sb3 = new StringBuilder("hello");
+        StringBuilder sb4 = new StringBuilder("hello");
+        StringBuilder sb5 = new StringBuilder("Hello");
+        boolean a = Lesson17.ravenstva(sb4,sb5);
+        System.out.println(a);
+        System.out.println(Lesson17.ravenstva(sb4,sb5));
+        System.out.println(Lesson17.ravenstva(new StringBuilder(""), new StringBuilder("")));
+        System.out.println(Lesson17.ravenstva(new StringBuilder(), new StringBuilder()));
+
+
+
+    }
+
+}
