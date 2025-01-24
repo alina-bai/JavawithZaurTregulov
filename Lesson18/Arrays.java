70dcf55c4b503d10b847ab4d45a7dfaf66289202
Alina-bai <alina.baishukurova@gmail.com>
2025-01-24 11:19:14 +0400

    Lesson18

---
+++ Lesson18/Arrays.java
@@ -0,0 +1,29 @@
+package org.example.Lesson18;
+
+public class Arrays {
+    public static void showArray(String[][] array) {
+        System.out.print("{  ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("{  ");
+            for (int j = 0; j < array[i].length; j++) { // Здесь исправлено array.length на array[i].length
+                if (j != array[i].length - 1) {
+                    System.out.print(array[i][j] + ", ");
+                } else {
+                    System.out.print(array[i][j]);
+                }
+            }
+            if (i != array.length - 1) {
+                System.out.print("},  ");
+            } else {
+                System.out.print("}");
+            }
+        }
+        System.out.println("   }");
+    }
+
+    public static void main(String[] args) {
+        String[][] array = {{"apple", "orange"}, {"hello", "bye", "ok"}, {"car"}};
+        showArray(array);
+        showArray(new String[][]{{"man", "woman"}, {"male", "female"}});
+    }
+}
